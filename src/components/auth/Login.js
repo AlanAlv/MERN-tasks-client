478d486fe3d629f69c373f3e3df0daaaa9625f9c
AlanAlv <alan.alv96@gmail.com>
2020-06-09 03:56:36 -0500

    Login form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,46 @@
 import React from 'react'
 
+
+const onChangeLogin = () => {
+
+}
 const Login = () => {
     return ( 
-        <h1>Login</h1>
+        <div className="form-user">
+            <div className="container-form shadow-dark">
+                <h1>Login</h1>
+                <form>
+                    <div className="field-form">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email" 
+                            name="email" 
+                            id="email"
+                            placeholder="Email"
+                            onChange={onChangeLogin}
+                            />
+                    </div>
+                    <div className="field-form">
+                        <label htmlFor="email">Password</label>
+                        <input 
+                            type="password" 
+                            name="password" 
+                            id="password"
+                            placeholder="Password"
+                            onChange={onChangeLogin}
+                            />
+                    </div>
+
+                    <div className="field-form">
+                        <input 
+                            type="submit" 
+                            className="btn btn-primary btn-block"
+                            value="Login"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
      );
 }
  
