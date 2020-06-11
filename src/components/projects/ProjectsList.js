e0429a830e5cb365590c8def68f4be2b3c48e468
AlanAlv <alan.alv96@gmail.com>
2020-06-11 03:12:32 -0500

    Projects state with context

--- src/components/projects/ProjectsList.js
+++ src/components/projects/ProjectsList.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import Project from './Project'
+import React, { useContext } from 'react';
+import Project from './Project';
+import projectContext from '../../context/projects/projectContext';
 
 const ProjectsList = () => {
 
-    const projects = [
-        {name: 'Virtual Store'},
-        {name: 'Web App'},
-        {name: 'Mobile App'}
-    ]
+    // Destructure projects from initial state
+    const projectsContext = useContext(projectContext);
+    const { projects } = projectsContext;
+
+    // Validate if there are projects
+    if (projects.length === 0) return null;
 
     return ( 
         <ul className="projects-list">
             {projects.map(project => (
                 <Project 
+                    key={project.id}
                     project={project}
                 />
             ))}
--- src/context/projects/projectState.js
+++ src/context/projects/projectState.js
@@ -6,6 +6,13 @@ import { PROJECT_FORM } from '../../types'
 
 const ProjectState = props => {
     const initialState = {
+        projects : [
+            {id: 1, name: 'Virtual Store'},
+            {id: 2, name: 'Web App'},
+            {id: 3, name: 'Mobile App'},
+            {id: 4, name: 'MERN'}
+        ],
+    
         form : false
     }
 
@@ -22,6 +29,7 @@ const ProjectState = props => {
     return(
         <projectContext.Provider
             value={{
+                projects: state.projects,
                 form: state.form,
                 showForm
             }}
